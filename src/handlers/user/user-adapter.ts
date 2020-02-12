20d58deb97665936b4f38cc86808c9b9242ac28a
bluechiptony <anthony.egwu@bizzdeskgroup.com>
2020-02-12 04:48:08 +0100

    added user function definitions

--- src/handlers/user/user-adapter.ts
+++ src/handlers/user/user-adapter.ts
@@ -2,3 +2,7 @@ import { Request, Response, NextFunction } from "express";
 
 export const createNewUser = (req: Request, res: Response) => {};
 export const updateNewUser = (req: Request, res: Response) => {};
+export const getAllUsers = (req: Request, res: Response) => {};
+export const getSingleUSer = (req: Request, res: Response) => {};
+export const searchUsersByName = (req: Request, res: Response) => {};
+export const searchUsersByAnyString = (req: Request, res: Response) => {};
