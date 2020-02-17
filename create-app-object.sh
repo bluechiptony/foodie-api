6f25b0db4def839f15d3bbceddd1e4a329df0cdd
bluechiptony <tony5egwu@gmail.com>
2020-02-17 22:11:49 +0100

    meal, meal vendor and rating handlers created

--- create-app-object.sh
+++ create-app-object.sh
@@ -3,6 +3,7 @@ touch src/apidoc/$1.js
 touch src/routes/$1.ts
 mkdir src/handlers/$1
 touch src/handlers/$1/$1.ts
+touch src/handlers/$1/$1.module.ts
 touch src/handlers/$1/$1-data-access.ts
 touch src/handlers/$1/$1-validator.ts
 touch src/handlers/$1/$1-adapter.ts
